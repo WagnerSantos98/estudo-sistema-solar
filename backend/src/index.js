042900ffd122319aabcd0fcf6aae31ec211fc8f4
WagnerSantos98 <wagner.jesus98@outlook.com>
2025-03-15 14:42:23 -0300

    refactor: alteração de rota

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const planetsRoutes = require('./routes/planets');
+const planetsRoutes = require('./routes/planetRoutes');
 
 dotenv.config();
 
