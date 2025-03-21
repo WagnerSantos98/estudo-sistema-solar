b2eb018c53cf0a87967f52b42fdbc9be73aa13d2
WagnerSantos98 <wagner.jesus98@outlook.com>
2025-03-21 14:37:01 -0300

    build: instalação do node_modules e run build para verificação

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    // Aumentar o limite de tamanho do chunk para 2MB (ou o valor que você achar adequado)
+    chunkSizeWarningLimit: 2000, // 2000 KB (2MB)
+
+    // Manual chunking
+    rollupOptions: {
+      output: {
+        // Dividir dependências de bibliotecas populares em um arquivo separado
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'], // Exemplo de dependências que podem ser separadas
+        },
+      },
+    },
+  },
+});
